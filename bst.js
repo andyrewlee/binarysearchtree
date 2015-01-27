0958496a4cd301860ffbb1b38c9ef892e306e481
Andrew Lee <andyrewlee@gmail.com>
2015-01-26 16:08:38 -0800

    Initial commit

---
+++ bst.js
@@ -0,0 +1,24 @@
+var Node = require('./node');
+
+var BST = (function(){
+  function BST() {
+    this.root = null;
+  };
+
+  BST.prototype.insert = function(data) {
+  };
+
+  BST.prototype.inOrder = function(node) {
+  };
+
+  BST.prototype.preOrder = function(node) {
+  };
+
+  BST.prototype.postOrder = function(node) {
+  };
+
+
+  return BST;
+})();
+
+module.exports = BST;
